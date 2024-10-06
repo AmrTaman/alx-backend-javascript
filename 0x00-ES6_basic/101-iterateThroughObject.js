c7b2e04743021175dfd1b84c4d6f5eafc35696e8
AmrTaman <amrtaman4325@gmail.com>
2024-10-06 22:49:17 +0000

    edf

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,8 @@
+export default function iterateThroughObject(reportWithIterator) {
+  let formated = `${reportWithIterator[0]} `;
+  reportWithIterator.shift();
+  for (const employee of reportWithIterator) {
+    formated += `| ${employee}`;
+  }
+  return formated;
+}
