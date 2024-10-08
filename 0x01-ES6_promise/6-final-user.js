1ece4235ab4df1f19174e9d5ce7313871e879b5b
AmrTaman <amrtaman4325@gmail.com>
2024-10-08 20:46:34 +0000

    async removed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,6 @@
 import uploadPhoto from './5-photo-reject';
-import signUpUser from "./4-user-promise";
+import signUpUser from './4-user-promise';
 
-export default async function handleProfileSignup(firstName, lastName, fileName){
-  return await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
 }
-
