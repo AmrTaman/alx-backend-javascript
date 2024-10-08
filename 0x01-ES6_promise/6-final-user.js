68164b5b23b2b1dd0f56b14ff9b4d488f3683c6e
AmrTaman <amrtaman4325@gmail.com>
2024-10-08 20:02:25 +0000

    test

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,7 @@
+import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+
+export default async function handleProfileSignup(firstName, lastName, fileName){
+  return await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+}
+
