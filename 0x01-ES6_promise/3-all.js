5c39bf227d41fdb071ac340637f5f4f0cc1b76d9
AmrTaman <amrtaman4325@gmail.com>
2024-10-08 18:12:18 +0000

    returning multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,22 +1,11 @@
 import * as utils from './utils';
 
 export default function handleProfileSignup() {
-  utils.uploadPhoto()
-    .then((val) => {
-      process.stdout.write(val.body);
+  return Promise.all([utils.uploadPhoto(), utils.createUser()])
+    .then((values) => {
+      console.log(`${values[0].body} ${values[1].firstName} ${values[1].lastName}`);
     })
     .catch(() => {
-      console.error('Signup system offline');
+      console.log('Signup system offline');
     });
-  utils.createUser()
-    .then((val) => {
-      // eslint-disable-next-line prefer-template
-      process.stdout.write(' ' + val.firstName);
-      // eslint-disable-next-line prefer-template
-      process.stdout.write(' ' + val.lastName + '\n');
-    })
-    .catch(() => {
-      console.error('Signup system offline');
-    });
-  return utils
 }
