6f21f28b56084eac5b6c80db8af0ab8f83167a84
AmrTaman <amrtaman4325@gmail.com>
2024-10-10 15:40:54 +0000

    ddd

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,9 @@
 import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return (await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]).then((values) => values));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const prom = Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+    return prom;
+  
 }
---
+++ 0x01-ES6_promise/6-main.js
@@ -0,0 +1,5 @@
+import handleProfileSignup from './6-final-user';
+
+const amr = handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg");
+console.log(amr);
+
