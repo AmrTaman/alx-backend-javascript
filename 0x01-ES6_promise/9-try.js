d9e4ef30da97f94f49a674769fc15b31a78fab46
AmrTaman <amrtaman4325@gmail.com>
2024-10-13 16:35:34 +0000

    catching errors

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,12 +1,9 @@
-import divideFunction from './8-try';
-
 export default function guardrail(mathFunction) {
   const lst = [];
   try {
-	  lst.push(mathFunction());
-  }
-  catch (error) {
-	  lst.push(String(error));
+    lst.push(mathFunction());
+  } catch (error) {
+    lst.push(String(error));
   }
   lst.push('Guardrail was processed');
   return lst;
