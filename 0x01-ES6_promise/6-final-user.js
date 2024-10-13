d6915106665cfd620c561ac1cdb943313d8bd6fb
AmrTaman <amrtaman4325@gmail.com>
2024-10-13 14:39:47 +0000

    we are here

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,9 @@
 import uploadPhoto from './5-photo-reject';
 import signUpUser from './4-user-promise';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const prom = Promise
-    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
-    return prom;
-  
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const prom1 = await signUpUser(firstName, lastName);
+  const prom2 = uploadPhoto(fileName);
+  const bogProm = await Promise.allSettled([prom1, prom2]);
+  return bogProm;
 }
