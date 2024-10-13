ce53b98d6431cc0683f7ac8c762a1a12a9f8d394
AmrTaman <amrtaman4325@gmail.com>
2024-10-13 18:23:57 +0000

    safe

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,16 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const val = {
-    photo: null,
-    user: null,
-  };
-  const prom1 = await uploadPhoto();
-  const prom2 = await createUser();
-  console.log(prom2);
-  if (prom1 && prom2) {
-    val.photo = prom1;
-    val.user = prom2;
+  let val = {};
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    val = { photo, user };
+  } catch (error) {
+    val = { photo: null, user: null };
   }
   return val;
 }
