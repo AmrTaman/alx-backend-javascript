ad020b90d2a9e940fed07dce80c7a8dfa5fc0b69
AmrTaman <amrtaman4325@gmail.com>
2024-10-22 00:16:44 +0000

    iam here doing setters and getters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -2,6 +2,9 @@ export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof (name) !== 'string') throw new TypeError('Name must be a string');
     if (typeof (length) !== 'number') throw new TypeError('Length must be a number');
+    if (!Array.isArray(students) || !students.every(s => typeof s === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
     this._name = name;
     this._length = length;
     this._students = students;
@@ -30,6 +33,9 @@ export default class HolbertonCourse {
   }
 
   set students(x) {
+    if (!Array.isArray(x) || !x.every(s => typeof s === 'string')) {
+	          throw new TypeError('Students must be an array of strings');
+	        }
     this._students = x;
   }
 }
