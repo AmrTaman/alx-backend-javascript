b1b5992edf1aff019953d915a576c15d3cae8511
AmrTaman <amrtaman4325@gmail.com>
2024-10-26 21:38:03 +0000

    imposter detected

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -2,7 +2,8 @@ export default class Building {
   constructor(sqft) {
     if (typeof sqft !== 'number') throw new TypeError('sqft must be a number');
     this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+    if (this.constructor !== Building
+            && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
